0f18ce99c2ce2f673bdd16e7f91aa43b1da94905
Sododa <sophieododa@gmail.com>
2024-09-03 10:29:39 +0000

    archaived

---
+++ encrypt_password.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" passwords.
+"""
+
+
+import logging
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hashes the provided pas
+    """
+    # Salt and hash the password using the bcrypt package
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates that the provided password matche.
+    """
+    # Try to match the hashed password with the given password
+    try:
+        return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    # If there is an exception in the process, log the error message
+    except Exception as e:
+        logging.error("Error in password validation: {}".format(e))
+        return False
