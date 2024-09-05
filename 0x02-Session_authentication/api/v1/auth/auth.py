4dcd7088fdaa412fe15934dc82ba2c6948285487
Sododa <sophieododa@gmail.com>
2024-09-05 12:18:51 +0000

    auth

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,54 +1,46 @@
 #!/usr/bin/env python3
+"""Authentication module for the API.
 """
-Mentication
-"""
+import os
+import re
 from typing import List, TypeVar
-
 from flask import request
 
 
-class Auth():
-    """Template system implemented in this app.
+class Auth:
+    """Authentication class.
     """
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """This function takes a path and a list of excluded paths as arguments
-        and returns a boolean val.
+        """Checks if a path requires authentication.
         """
-        # If path is None, return True
-        if not path:
-            return True
-        # If _paths is None or empty, return True
-        if not excluded_paths:
-            return True
-        # Remove theg slash from the path
-        path = path.rstrip("/")
-        # Check if path is is and return False if path is
-        # ind_paths
-        # Loop d paths
-        for excluded_path in excluded_paths:
-            # Check if givenexcluded path, with * at the end
-            if excluded_path.endswith("*") and \
-                    path.startswith(excluded_path[:-1]):
-                # Return False excluded path with * at end
-                return False
-            # Check if the given excluded path
-            elif path == excluded_path.rstrip("/"):
-                # Return Fath
-                return False
-        # If
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        """Gets the value of the Authorization header from the request
+        """Gets the authorization header field from the request.
         """
-        # If request is None, return None
-        # If request doesn’t contain the header key Authorization, return None
         if request is not None:
             return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """This function takes a request object as an optional argument.
+        """Gets the current user from the request.
         """
         return None
+
+    def session_cookie(self, request=None) -> str:
+        """Gets the value of the cookie named SESSION_NAME.
+        """
+        if request is not None:
+            cookie_name = os.getenv('SESSION_NAME')
+            return request.cookies.get(cookie_name)
